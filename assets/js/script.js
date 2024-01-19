fb769aaa3ee99e7d25dff09d3562b29d9419758a
Adrien Miko <mikolajczykadrien@gmail.com>
2024-01-19 18:13:32 +0100

    ajout cv

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// let lastScrollPosition = window.scrollY;
+// window.addEventListener("scroll", () => {
+//   const currentScrollPosition = window.scrollY;
+//   if (currentScrollPosition > lastScrollPosition) {
+//     navbar.style.top = `-${navbar.offsetHeight}px`;
+//   } else {
+//     navbar.style.top = 0;
+//   }
+// });
+//
