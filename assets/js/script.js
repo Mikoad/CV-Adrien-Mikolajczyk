12bfee885439276cd6b8cd6913d2719d174ce6ed
Adrien Miko <mikolajczykadrien@gmail.com>
2024-01-26 16:24:07 +0100

    modifications responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,17 @@ burgerMenu.addEventListener("click", () => {
 
 // const competencesDev = document.getElementById("competencesDev");
 // const competencesTools = document.getElementById("competencesTools");
+// window.addEventListener("scroll", () => {
+//   let scrollValue =
+//     (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+//   if (scrollValue > 0.4) {
+//     competencesDev.style.transform = "translateX(0)";
+//     competencesDev.style.opacity = 1;
+//     competencesTools.style.transform = "translateX(0)";
+//     competencesTools.style.opacity = 1;
+//   }
+// });
+
+//competencesTools.style.transform =
+//    competencesDev.classList.add("displayCompetences");
+//competencesTools.classList.add("displayCompetences");
