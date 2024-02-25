4fca1c9df0192fd1d9b594305fa21de5951c0f98
Adrien Miko <mikolajczykadrien@gmail.com>
2024-02-25 16:27:19 +0100

    ajout projet Cozy Page Produit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 const burgerMenu = document.getElementById("burgerMenu");
 const sidebar = document.getElementById("sidebar");
-
+const homePage = document.querySelector(".homePage");
 burgerMenu.addEventListener("click", () => {
   sidebar.classList.toggle("displaySidebar");
   burgerMenu.classList.toggle("displaySidebar");
 });
+homePage.addEventListener("click", () => {
+  sidebar.classList.remove("displaySidebar");
+  burgerMenu.classList.remove("displaySidebar");
+});
 
+//----------display competences -----------
 // const competencesDev = document.getElementById("competencesDev");
 // const competencesTools = document.getElementById("competencesTools");
 // window.addEventListener("scroll", () => {
@@ -22,3 +27,38 @@ burgerMenu.addEventListener("click", () => {
 //competencesTools.style.transform =
 //    competencesDev.classList.add("displayCompetences");
 //competencesTools.classList.add("displayCompetences");
+
+//------Type Writer-------------
+// const target = document.getElementById("target");
+// let array = ["développeur", "passionné"];
+// let wordIndex = 0;
+// let letterIndex = 0;
+
+// const createLetter = () => {
+//   const letter = document.createElement("span");
+//   target.appendChild(letter);
+//   letter.textContent = array[wordIndex][letterIndex];
+//   setTimeout(() => {
+//     letter.remove();
+//   }, 2000);
+// };
+
+// const loop = () => {
+//   setTimeout(() => {
+//     if (wordIndex >= array.length) {
+//       wordIndex = 0;
+//     } else if (letterIndex < array[wordIndex].length) {
+//       createLetter();
+//       letterIndex++;
+
+//       loop();
+//     } else {
+//       wordIndex++;
+//       letterIndex = 0;
+//       setTimeout(() => {
+//         loop();
+//       }, 2000);
+//     }
+//   }, 80);
+// };
+// loop();
